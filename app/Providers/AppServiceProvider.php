bdd9875c0355bd6045fb8875a6a4e404374b766f
Nagase-123 <121854287+Nagase-123@users.noreply.github.com>
2023-08-04 14:32:28 +0900

    Update AppServiceProvider.php
    https通信用

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         /**
+         * .envファイルの(APP_ENV=production)のとき、強制https化
+         */
+        if(\App::environment('production')) {
+            \URL::forceScheme('https');
+            
     }
 }
