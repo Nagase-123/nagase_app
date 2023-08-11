7f4b00b4fc268a107a6a628bb70134e09cdf5e13
Nagase <nyo_ino_same@yahoo.com>
2023-08-11 19:25:16 +0900

    パスワード再設定後セッションリセット

--- resources/views/login_logout/pass_reset_confirm.blade.php
+++ resources/views/login_logout/pass_reset_confirm.blade.php
@@ -28,7 +28,6 @@
   }
   @endphp
 
-  <p>パスワードを入力後、送信ボタンを押してください。</p>
   <p>{{$anser}}</p>
   @if(isset($msg))
   <p class="error_msg">※{{$msg}}</p>
@@ -46,12 +45,12 @@
 
   <div class="Form-Item">
     <p class="Form-Item-Label">パスワード</p>
-    <input type="password" name="password" id="pass" class="Form-Item-Input" value="{{old('password')}}" required>
+    <input type="password" name="password" id="pass" class="Form-Item-Input" value="{{old('password')}}" required placeholder="※１０文字以上 半角英数字で入力">
   </div>
 
   <div class="Form-Item">
     <p class="Form-Item-Label">パスワード確認</p>
-    <input type="password" name="password_confirmation" class="Form-Item-Input" value="{{old('password_confirmation')}}" required>
+    <input type="password" name="password_confirmation" class="Form-Item-Input" value="{{old('password_confirmation')}}" required placeholder="※１０文字以上 半角英数字で入力">
   </div>
 
   <button type="submit" class="Form-Btn" onclick="return alert_js()">送信</button>
